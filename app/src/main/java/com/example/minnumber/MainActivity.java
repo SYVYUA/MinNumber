d4eca2f4bccb7a0a6e014d9cb12fcbeb5aefaa21
Dmitry <sivokon94@gmail.com>
2020-01-07 14:12:47 +0200

    start screen

--- app/src/main/java/com/example/minnumber/MainActivity.java
+++ app/src/main/java/com/example/minnumber/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.minnumber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button startBut;
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startBut = findViewById(R.id.startBut);
+        startBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(MainActivity.this, MenuGame.class);
+                startActivity(intent);
+            }
+        });
     }
 }
