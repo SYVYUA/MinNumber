8513de9795286e62fbf551d81f75bc4c18963592
Dmitry <sivokon94@gmail.com>
2020-01-09 09:39:01 +0200

    button for lvl

--- app/src/main/java/com/example/minnumber/MenuGame.java
+++ app/src/main/java/com/example/minnumber/MenuGame.java
@@ -3,12 +3,37 @@ package com.example.minnumber;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MenuGame extends AppCompatActivity {
+
+    private Button one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen, twenty, twenty_one;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_game);
+
+        one = findViewById(R.id.one);
+        two = findViewById(R.id.two);
+        three = findViewById(R.id. three);
+        four = findViewById(R.id.four);
+        five = findViewById(R.id.five);
+        six = findViewById(R.id.six);
+        seven = findViewById(R.id.seven);
+        eight = findViewById(R.id.eight);
+        nine = findViewById(R.id.nine);
+        ten = findViewById(R.id.ten);
+        eleven = findViewById(R.id.eleven);
+        twelve = findViewById(R.id.twelve);
+        thirteen = findViewById(R.id.thirteen);
+        fourteen = findViewById(R.id.fourteen);
+        fifteen = findViewById(R.id.fifteen);
+        sixteen = findViewById(R.id.sixteen);
+        seventeen = findViewById(R.id.seventeen);
+        eighteen = findViewById(R.id.eighteen);
+        nineteen = findViewById(R.id.nineteen);
+        twenty = findViewById(R.id.twenty);
+        twenty_one = findViewById(R.id.twenty_one);
     }
 }
